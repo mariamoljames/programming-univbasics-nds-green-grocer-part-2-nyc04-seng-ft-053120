2daa8f4783b5f72959f3370bf7366feee7a645bb
Mariamol James <mariamol.james@gmail.com>
2020-06-21 01:38:56 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -48,10 +48,9 @@ def checkout(cart, coupons)
   consolidated=consolidate_cart(cart)
   coupon=apply_coupons(consolidated,coupons)
   clearance=apply_clearance(coupon)
-  consolidated.each do |x|
-    clearance.each do |y|
-      coupons.each do |z|
-        answer-=x[:price]*x[:count]
+    clearance.each do |x|
+      coupons.each do |y|
+        answer+=x[:price]*x[:count]
         if x[:item].include?("DISCOUNT") && x[:count]<y[:num]
           answer-=x[:price]*x[:count]
         end
